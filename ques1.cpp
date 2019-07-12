a73bc76b85b92a5279052d6ac39261bd85828e4b
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-07-12 11:42:26 +0530

    Create ques1.cpp

---
+++ ques1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string name;
+    int k;
+    cin>>name>>k;
+    for(int i=0;i<k;i++)
+    {
+        cout<<name<<"\n";
+    }
+    return 0;
+}
