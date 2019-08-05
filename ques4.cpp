6542f887d2340ff9aa94f5215127a6c2baa75a6c
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-05 20:50:43 +0530

    Create ques4.cpp

---
+++ ques4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cin>>number;
+    if(number>0 && number<10)
+        {
+      cout<<"yes";
+        }
+        else 
+        {
+            cout<<"no";
+        }
+    
+ return 0;
+} 
