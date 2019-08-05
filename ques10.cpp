a0f8b41e9031d00c022bbf5674a2ec60692d3419
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-05 22:30:25 +0530

    Create ques10.cpp

---
+++ ques10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number,a,b;
+    cin >> number;
+   a=number/2;
+   if(a%2==0){
+       cout<<"yes";
+   }
+   else
+   {
+       cout<<"no";
+   }
+    return 0;
+}
